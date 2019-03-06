ec57a76aa7e2c6c3fb403c2f38e553738cf19b2b
Nikolaj <Nikolaj@DESKTOP-N64NHMQ>
2019-03-06 15:19:23 +0100

    implementering af stack class

---
+++ Ex57_StacksAndQueues/MyStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex57_StacksAndQueues
+{
+    public class MyStack<T>
+    {
+        private List<T> stack;
+        public MyStack()
+        {
+            stack = new List<T>();
+        }
+
+        public void Push(T item)
+        {
+            stack.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                return default(T);
+            }
+            else
+            {
+                T data = stack.Last();
+                stack.Remove(stack.Last());
+                return data;
+            }
+        }
+
+        public T Peek()
+        {
+            return stack.Last();
+        }
+
+        public int Count()
+        {
+            return stack.Count();
+        }
+
+        public Boolean IsEmpty()
+        {
+            if (Count() == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -48,5 +48,45 @@ namespace UnitTestProject1
             // Test queue is empty now
             Assert.AreEqual(true, ourQueue.IsEmpty());
         }
+        [TestMethod]
+        public void TestStack()
+        {
+            Stack<int> stdStack = new Stack<int>();   // standard queue implemented by .NET
+            MyStack<int> ourStack = new MyStack<int>();  // our manually implemented stack
+
+            // Test generates 100 random integers and adds to both queues
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int randVal = r.Next();
+                stdStack.Push(randVal);
+                ourStack.Push(randVal);
+            }
+
+            // Test reading back half the added integers
+            for (int i = 0; i < 50; i++)
+            {
+                Assert.AreEqual(stdStack.Pop(), ourStack.Pop());
+                Assert.AreEqual(stdStack.Peek(), ourStack.Peek());
+                Assert.AreEqual(stdStack.Count, ourStack.Count());
+            }
+
+            // Test adding 50 more random integers
+            for (int i = 0; i < 50; i++)
+            {
+                int randVal = r.Next();
+                stdStack.Push(randVal);
+                ourStack.Push(randVal);
+            }
+
+            // Test reading back all the remaining values
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(stdStack.Peek(), ourStack.Peek());
+                Assert.AreEqual(stdStack.Pop(), ourStack.Pop());
+                Assert.AreEqual(stdStack.Count, ourStack.Count());
+            }
+        }
+
     }
 }
