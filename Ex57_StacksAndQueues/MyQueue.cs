265ed1248631c281fa7c5bfba4f4efdfd188a426
Nikolaj <Nikolaj@DESKTOP-N64NHMQ>
2019-03-06 15:09:07 +0100

    implementeret MyQueue class

---
+++ Ex57_StacksAndQueues/MyQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex57_StacksAndQueues
+{
+    public class MyQueue<T>
+    {
+        // Insert local datastructures here ...
+        private List<T> queue;
+        public MyQueue()
+        {
+            queue = new List<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            queue.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                return default(T);
+            }
+            else
+            {
+                T data = queue[0];
+                queue.RemoveAt(0);
+                return data;
+            }
+        }
+
+        public int Count()
+        {
+            return queue.Count();
+        }
+
+        public Boolean IsEmpty()
+        {
+            if (Count() == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2a213c7-48af-440d-8e6a-1ed5e350303d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
